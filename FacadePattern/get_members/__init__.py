0f31223b39c37f194c183b7028563b36b6ff2e16
GreenManSteve <74726965+GreenManSteve@users.noreply.github.com>
2024-10-10 12:25:10 +0100

    Delete FacadePattern directory

--- FacadePattern/get_members/__init__.py
+++
@@ -1,7 +0,0 @@
-SQL_PROVIDER = "sql_server"
-ORACLE_PROVIDER = "oracle_provider"
-
-CONSSTR = ""
-
-
-QUERY = ""
--- FacadePattern/get_members/oracle_provider.py
+++
@@ -1,6 +0,0 @@
-from .abs_facade import AbsFacade
-
-
-class GetMembers(AbsFacade):
-    def get_employees(self):
-        print("Bill, Shelly, Rene")
